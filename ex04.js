faf4494ac5205f09a101032b6b304d91a80dbee2
Michael Behrens <michael.behrens33@gmail.com>
2017-03-18 19:43:39 +0100

    ok

--- ex04.js
+++ ex04.js
@@ -14,3 +14,15 @@
 */
 
 // write your code below this comment
+
+console.log(findSumByMultiples());
+
+function findSumByMultiples(){
+    var tot=0;
+    for(let i=0;i<1000;i++){
+        if (((i%3)==0) || ((i%5)==0)){
+            tot+=i;
+        } 
+    }
+    return(tot);
+}
