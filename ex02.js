db4cdef69519b54ecea33fe9c7c3952500118d66
Michael Behrens <michael.behrens33@gmail.com>
2017-03-18 19:00:59 +0100

    Temperature aleatoire, Param en min ou maj + non gere, reference centigrade, division par 100 pour cadre sur 2 chiffe apres virgule

--- ex02.js
+++ ex02.js
@@ -15,5 +15,19 @@
     Write your own tests !
     
 */
+console.log(randomTemperatur('C'));
+console.log(randomTemperatur('f'));
+console.log(randomTemperatur(''));
+
 
 // write your code below this comment
+function randomTemperatur(param){
+    var tempCelcius=(Math.trunc(Math.random()*10000))/100;
+    if (param.toUpperCase()=="C") {
+        return(tempCelcius+"°C")
+    } else if (param.toUpperCase()=="F"){
+        return( ((Math.trunc( ((tempCelcius * 1.8) + 32) * 100)/100) )+"°F" );
+    } else {
+        return(" echelle d temperature non gere");
+    }
+}
