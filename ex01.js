f57ca540a6ebe9363e59d98304d342959879858c
Michael Behrens <michael.behrens33@gmail.com>
2017-03-18 18:25:19 +0100

    exercice1

--- ex01.js
+++ ex01.js
@@ -27,3 +27,74 @@
 */
 
 // write your code below this comment
+
+
+//evalExpr(4, 5, "*");
+
+    evalExpr(4, 5, "*"); // 20
+    evalExpr(3, 3, "+"); // 6
+    evalExpr(2, 2, "%"); // 0
+    evalExpr(3, 3, "-"); // 0
+    evalExpr(10, 2, "/"); // 5
+
+    evalExpr(10, 0, "/"); // 0
+
+function evalExpr(param1,param2,ope){
+    var result;
+
+    
+    if (ope=="+"){
+            result=addition(param1,param2);
+    } else if (ope=="-"){
+            result=soustraction(param1,param2);
+    } else if (ope=="*"){
+            result=multiplication(param1,param2);      
+    } else if (ope=="/"){
+            result=division(param1,param2);
+    } else if (ope=="%"){
+            result=modulo(param1,param2);
+    }
+
+
+    affiche(result);
+}
+
+function addition(param1,param2){
+    var ret=param1+param2;
+    return(ret);
+}
+
+function affiche(ret){
+    console.log(ret);
+}
+
+function soustraction(param1,param2){
+    var ret=param1-param2;
+    return(ret);
+}
+
+
+function multiplication(param1,param2){
+    var ret=param1*param2;
+    return(ret);
+}
+
+
+function division(param1,param2){
+    var ret;
+    if (param2==0){
+        ret=0;
+    } else {
+    ret=param1/param2;        
+    }
+    return(ret);
+}
+
+
+function modulo(param1,param2){
+    var ret=0;
+    var entier=Math.trunc(division(param1,param2));
+    ret=param1-(entier * param2);
+    return(ret);
+}
+
