37a5a2f7252a6e954f78a68f56b6302cfa16aae9
Michael Behrens <michael.behrens33@gmail.com>
2017-03-18 19:30:29 +0100

    toutes les reponses sont dans les tests

--- ex03.js
+++ ex03.js
@@ -21,3 +21,30 @@
 */
 
 // write your code below this comment
+
+console.log(number2Price(27));
+console.log(price2Number("4.87€"));
+console.log(number2Price(""));
+console.log(price2Number(""));
+
+console.log(Math.sign(0));
+console.log(Math.sign(-0));
+
+
+function number2Price(param){
+    if ((param<0) || (param=="")){
+        return((param<0)?0:"");
+    }
+    return((param * 100 / 100)+"€");
+}
+
+function price2Number(param){
+    var nombre=parseFloat(param);
+    if (isNaN(nombre)){
+        return("");
+    }
+    if ((nombre<0) || (nombre=="")){
+        return((nombre<0)?0:"");
+    }
+    return(nombre);
+}
